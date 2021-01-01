5319529f7494043d2bf5b0c14ed030c62ac77e44
ThatcherK <kaliithatmau@gmail.com>
2021-01-01 13:18:30 +0300

    Add register business form

--- src/components/admin/registerBusiness.js
+++ src/components/admin/registerBusiness.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import instance from '../../config/axiosConfig'
 
 export default function RegisterBusiness(){
+
+    const formik = useFormik({
+        initialValues: {
+            name: ''
+        },
+        validationSchema: Yup.object({
+            name: Yup.string().required('Required!'),
+        }),
+        onSubmit: (values, onSubmitProps) => {
+            handleRegister();
+            onSubmitProps.resetForm()
+        }
+    });
+
+    const payload = {
+        name: formik.values.name
+    }
+    console.log(payload)
+    const handleRegister = () => {
+
+        instance
+            .post('/business', payload)
+            .then((response) => {
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error.response.data)
+            })
+    }
     return (
         <div>
-            
+            <form onSubmit={formik.handleSubmit}>
+                <input placeholder="Enter Business Name" name="name" onChange={formik.handleChange} value={formik.values.name}/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
