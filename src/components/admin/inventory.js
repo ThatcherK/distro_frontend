b07a979a9748596a0697c8abeb707be67f42c132
ThatcherK <kaliithatmau@gmail.com>
2021-01-01 16:27:20 +0300

    Add inventory api call

--- src/components/admin/inventory.js
+++ src/components/admin/inventory.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import instance from '../../config/axiosConfig'
 
 export default function Inventory(){
+    const [inventory,setInventory] = useState([])
+
+    useEffect(()=>{
+        instance.get('/inventory')
+        .then((response)=>{
+            console.log(response.data)
+            setInventory(response.data)
+        })
+    },[])
     return (
         <div>
             
